57debedbf26bc07c8b512a49d8759035a49729ad
Sri Hari Jagan <100404902+SriHariJagan@users.noreply.github.com>
2026-03-31 10:20:30 +0530

    Update seedProducts.js

--- seedProducts.js
+++ seedProducts.js
@@ -3,7 +3,7 @@ const Product = require("./models/Product"); // path to your Product model
 const products = require("./trimmedProducts.json"); // the JSON above
 
 mongoose
-  .connect("mongodb+srv://maanjoofarms_db_user:2WJAUwXsjWIhDQAQ@cluster0.k1vwfxb.mongodb.net/maanjufarms_db?retryWrites=true&w=majority", {
+  .connect("", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
